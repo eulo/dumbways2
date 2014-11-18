df73d3a4a91a72f5adb1a106d36fe0a8cccc00be
eulo <hello@will-pringle.com>
2014-11-18 06:43:43 +0000

    moar animationz!

--- js/src/characters.js
+++ js/src/characters.js
@@ -23,7 +23,7 @@ define(function(require, exports, module) {
     }
     // css animation
     setTimeout(function(){
-      $this.addClass(data.class);
+      //$this.addClass(data.class);
     }, Math.random() * 1000);
 
     var img = new Image();
@@ -32,16 +32,27 @@ define(function(require, exports, module) {
         // json data
         $.get(data.json).done(function(res) {
           var i = 0
-            , l = res.frames.length;
+            , l = res.frames.length
+            , dir = true;
+
           setInterval(function() {
             $this.css({
               'width': res.frames[i].frame.w + 'px',
               'height': res.frames[i].frame.h + 'px',
               'background-position': -res.frames[i].frame.x + 'px ' + -res.frames[i].frame.y +'px'
             }); 
-            i++;
-            if (i >= l) 
-              i = 0; 
+            if (data.alternate === true) {
+              if (dir === true && i + 1 >= l) {
+                dir = false;
+              } else if (dir === false && i - 1 < 0) {
+                dir = true;
+              }
+              i = dir ? i + 1 : i - 1;
+            } else {
+              i++;
+              if (i >= l) 
+                i = 0; 
+            }
           }, 1000 / data.fps );
         });
 
@@ -49,22 +60,31 @@ define(function(require, exports, module) {
         // simple animation
         data.maxHeight = this.height;
 
-        setInterval(function() {
+        setTimeout(function() {
+          setInterval(function() {
 
-          if (pos - data.height <= 0) {
-            dir = true;
-          } else if (pos + data.height >= data.maxHeight )
-            dir = false;
+            if (pos - data.height <= 0) {
+              if (data.reverse === true)
+                pos = data.maxHeight + data.height;
+              else
+                dir = true;
+            } else if (pos + data.height >= data.maxHeight ) {
+              if (data.reverse === false)
+                pos = 0 - data.height;
+              else
+                dir = false;
+            }
 
-          if (dir)
-            pos += data.height;
-          else 
-            pos -= data.height;
+            if (dir)
+              pos += data.height;
+            else 
+              pos -= data.height;
 
-          $this.css({
-            'background-position': '0px ' + pos + 'px'
-          }); 
-        }, 1000 / data.fps);
+            $this.css({
+              'background-position': '0px ' + -pos + 'px'
+            }); 
+          }, 1000 / data.fps);
+        }, Math.random() * 1000);
       }
     };
     img.src = data.file;
